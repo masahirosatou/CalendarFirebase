84cafe055669b65d0ea9e2db748fdc9fbf5a543e
佐藤正洋 <>
2020-06-09 09:59:24 +0900

    first commit

---
+++ app/src/main/java/com/example/calendarfirebase/AddCalendarActivity.java
@@ -0,0 +1,14 @@
+package com.example.calendarfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_calendar);
+    }
+}
